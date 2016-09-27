55799a3e0752891d2c22108aca419f16b118cab1
Michela Negro <mnegro@fermi.to.infn.it>
2016-09-27 17:23:26 +0200

    add mask file as config param

--- config/restyle_config.py
+++ config/restyle_config.py
@@ -12,19 +12,17 @@
 
 import os
 from GRATools import FT_DATA_FOLDER
+from GRATools import GRATOOLS_CONFIG
 
-
-IN_LABELS_LIST = ['1yr_UCV_t2', '2yr_UCV_t2',
-                  '3yr_UCV_t2', '4yr_UCV_t2',
-                  '5yr_UCV_t2', '6yr_UCV_t2',
-                  '7yr_UCV_t2', '8yr_UCV_t2'] 
-OUT_LABEL = 'Allyrs_UCV_t2'
+IN_LABELS_LIST = ['1yr_UCV_t0', '2yr_UCV_t0',
+                  '3yr_UCV_t0', '4yr_UCV_t0',
+                  '5yr_UCV_t0', '6yr_UCV_t0',
+                  '7yr_UCV_t0', '8yr_UCV_t0'] 
+OUT_LABEL = 'Allyrs_UCV_t0_srcmask2'
 BINNING_LABEL = '13bins'
 MICRO_NBINS = 100
 MACRO_BINS = [(18,24),(25,30),(31,35),(36,41),(42,47),(48,53),(54,58),(59,64),(65,70),(71,76),(77,81),(82,87),(88,93)]
 POWER_LOW_INDEX = 2.30
-NSIDE = 512
-SRC_CATALOG = os.path.join(FT_DATA_FOLDER,'catalogs/gll_psc_v16.fit')
-SRC_MASK_RAD = 1.5 #[deg]
-GP_MASK_LAT = 30.
+MASK_FILE = os.path.join(GRATOOLS_CONFIG, 'fits/Mask_src2_gp30.fits')
+
 
