541e80cb82702dfc2f996cf11ac5cc2f83dc3476
Michela Negro <michela.negro@to.infn.it>
2017-06-14 10:20:34 +0200

    implemented Wbeam integral in E bins v0

--- bin/mkpolspicecl.py
+++ bin/mkpolspicecl.py
@@ -57,12 +57,21 @@ def mkCl(**kwargs):
     """                                      
     """
     get_var_from_file(kwargs['config'])
+    
+    lmax = data.LMAX
+    _l = np.arange(0, lmax)
+    gamma = data.WEIGHT_SPEC_INDEX
+    out_label = data.OUT_LABEL
+    logger.info('Calculating energy spectrum...')
+    from GRATools.utils.gWindowFunc import get_powerlaw_spline
+    energy_spec = get_powerlaw_spline(gamma)
+
     logger.info('Calculating PSF with gtpsf...')
     dict_gtpsf = data.DICT_GTPSF
     logger.info('Calculating Wbeam Function...')
-    out_wb_label = data.OUT_W_LABEL
-    
+    out_wb_label = data.OUT_W_LABEL 
     out_wb_txt = os.path.join(GRATOOLS_OUT, 'Wbeam_%s.txt'%out_wb_label)
+    in_label = data.IN_LABEL
     if not os.path.exists(out_wb_txt):
         from GRATools.utils.ScienceTools_ import gtpsf
         gtpsf(dict_gtpsf)
@@ -73,15 +82,11 @@ def mkCl(**kwargs):
         from GRATools.utils.gWindowFunc import build_wbeam
         wb = build_wbeam(psf, _l, out_wb_txt)
     else:
-        from GRATools.utils.gWindowFunc import get_wbeam
-        wb = get_wbeam(out_wb_txt)
-    save_current_figure('Wbeam_%s.png'%out_wb_label, clear=True)
+        pass
 
     logger.info('Starting Cl analysis...')
-    in_label = data.IN_LABEL
     mask_label = data.MASK_LABEL
     in_label = in_label + '_' + mask_label
-    out_label = data.OUT_LABEL
     binning_label = data.BINNING_LABEL
     mask_file = data.MASK_FILE
     cl_param_file = os.path.join(GRATOOLS_OUT, '%s_%s_parameters.txt' \
@@ -90,22 +95,24 @@ def mkCl(**kwargs):
     _emin, _emax, _emean, _f, _ferr, _cn, _fsky = get_cl_param(cl_param_file)
     cl_txt = open(os.path.join(GRATOOLS_OUT, '%s_%s_polspicecls.txt' \
                                    %(out_label, binning_label)), 'w')
+    from GRATools.utils.gWindowFunc import get_integral_wbeam
     for i, (emin, emax) in enumerate(zip(_emin, _emax)):
         logger.info('Considering bin %.2f - %.2f ...'%(emin, emax))
         mask_f = mask_file
         if type(mask_file) == list:
             mask_f = mask_file[i]
-        gamma = data.WEIGHT_SPEC_INDEX
-        Im = (1/(1-gamma))*(emax**(1-gamma)-emin**(1-gamma))/(emax-emin)
-        eweightedmean = np.power(1/Im, 1/gamma)
-        cl_txt.write('ENERGY\t %.2f %.2f %.2f\n'%(emin, emax, eweightedmean))
-        l_max= 1000
+        mask = hp.read_map(mask_f)
+        wb1_en = get_integral_wbeam(out_wb_txt1, energy_spec, emin, emax)
+        cl_txt.write('ENERGY\t %.2f %.2f %.2f\n'%(emin, emax, _emean[i]))
+        l_max= lmax
         _l = np.arange(l_max)
-        wb_en = wb.hslice(eweightedmean)(_l)
+        wb_en = wb_en(_l)
         flux_map_name = in_label+'_flux_%i-%i.fits'%(emin, emax)
         flux_map_f = os.path.join(GRATOOLS_OUT_FLUX, flux_map_name)
-        flux_map = hp.read_map(flux_map_f)
+        flux_map_f_mdclean = remove_monopole_dipole(flux_map_f)
+        flux_map = hp.read_map(flux_map_f_mdclean)
         fsky = _fsky[i]
+        cn = _cn[i]
         if kwargs['show'] == True:
             hp.mollview(flux_map_masked.filled(), title='f$_{sky}$ = %.3f'%fsky,
                         min=1e-7, max=1e-4, norm='log')
@@ -115,6 +122,8 @@ def mkCl(**kwargs):
         wpix = hp.sphtfunc.pixwin(nside)[:l_max]
         out_name = '%s_%i-%i' %(out_label, emin, emax)
         out_folder =  os.path.join(GRATOOLS_OUT, 'output_pol')
+        logger.info('cn poisson = %e'%cn)
+        wl = wb_en*wpix
         if not os.path.exists(out_folder):
             os.makedirs(out_folder)
         pol_dict = data.POLCEPICE_DICT
@@ -130,16 +139,30 @@ def mkCl(**kwargs):
             if key == 'maskfile':
                 pol_dict[key] = mask_f
         config_file_name = 'pol_%s'%(out_name)
-        _l, _cl, _cl_err = pol_cl_calculation(pol_dict, config_file_name)        
-        logger.info('cn poisson = %e'%_cn[i])
-        cn = _cn[i]
-        wl = wb_en*wpix
-        _cl = (_cl[:l_max] - cn)/(wl**2)
-        _cl_err = _cl_err[:l_max]/(wl**2)
+        if os.path.exists(os.path.join(out_folder,'%s_cl.txt'%out_name)) and \
+                os.path.exists(os.path.join(out_folder,'%s_cov.fits'%out_name)):
+            logger.info('ATT: Retriving power spectrum...')
+            _l, _cl, _clerr = pol_cl_parse(os.path.join(out_folder,
+                                                        '%s_cl.txt'%out_name),
+                                           os.path.join(out_folder,
+                                                        '%s_cov.fits'%out_name),
+                                           raw_corr=(cn, wl),
+                                           rebin=True)
+            logger.info('... and covariance matrix.')
+            _cov = pol_cov_parse(os.path.join(out_folder,
+                                              '%s_cov.fits'%out_name),
+                                 wl_array=wl,
+                                 rebin=True, show=True)
+        else:
+            _l, _cl, _cl_err = pol_cl_calculation(pol_dict, 
+                                                  config_file_name,
+                                                  raw_corr=(cn, wl),
+                                                  rebin=True,show=True) 
+        cl_txt.write('multipole\t%s\n'%str(list(_l)).replace('[',''). \
+                         replace(']','').replace(', ', ' '))
         cl_txt.write('Cl\t%s\n'%str(list(_cl)).replace('[',''). \
                          replace(']','').replace(', ', ' '))
-        #_cl_err = np.sqrt(2./((2*_l+1)*fsky))*(_cl+(cn/wl**2))
-        cl_txt.write('Cl_ERR\t%s\n\n'%str(list(_cl_err)).replace('[',''). \
+        cl_txt.write('Cl_ERR\t%s\n\n'%str(list(_clerr)).replace('[',''). \
                          replace(']','').replace(', ', ' '))
     cl_txt.close()
     logger.info('Created %s'%(os.path.join(GRATOOLS_OUT,'%s_%s_polspicecls.txt'
