3322084b1285dd748a3fccb35887de1b4447ebb4
Michela Negro <michela.negro@to.infn.it>
2017-07-24 11:06:18 +0200

    simulated 3fgl Source map builder

--- utils/gSourceTemplate.py
+++ utils/gSourceTemplate.py
@@ -18,18 +18,41 @@ import os
 import healpy as hp
 import numpy as np
 import pyfits as pf
-from GRATools import GRATOOLS_CONFIG
+import pickle as pc
+from numba import jit
+from GRATools import GRATOOLS_CONFIG, GRATOOLS_OUT
 from GRATools.utils.logging_ import logger, abort
 from GRATools.utils.matplotlib_ import pyplot as plt
-from GRATools.utils.gWindowFunc import get_psf_ref
+from GRATools.utils.gWindowFunc import get_psf
+from GRATools.utils.gSpline import xInterpolatedUnivariateSplineLinear
 
-def psf_en(psf_file, energy):
-    psf_spline = get_psf_ref(psf_file)
-    psf_energy = psf_spline(energy)
-    return psf_energy
-    
+_ENERGY = np.logspace(1, 6,1000)
 
-def buils_src_template(cat_file, psf_file, energy, NSIDE):
+def LogParabola(norm, index1, index2, pivot_energy):
+    """Returns a spline of a LogParabola spectrum with given parameters
+    """
+    _spec = norm*(_ENERGY/pivot_energy)**(-index1-index2*np.log( \
+            _ENERGY/pivot_energy))
+    _spec_spline = xInterpolatedUnivariateSplineLinear(_ENERGY, _spec)
+    return _spec_spline
+
+def PLSuperExpCutoff(norm, index, pivot_energy, index_cut, energy_cut):
+    """Returns a spline of a Powerlaw with exponential cutoff spectrum with 
+       given parameters
+    """
+    _spec = norm*(_ENERGY/pivot_energy)**(-index)*np.exp((pivot_energy/ \
+             energy_cut)**(-index_cut)-(_ENERGY/pivot_energy)**(-index_cut))
+    _spec_spline = xInterpolatedUnivariateSplineLinear(_ENERGY, _spec)
+    return _spec_spline
+
+def PowerLaw(norm, index, pivot_energy):
+    """Returns a spline of a Powerlaw spectrum with given parameters
+    """
+    _spec = norm*(_ENERGY/pivot_energy)**(-index)
+    _spec_spline = xInterpolatedUnivariateSplineLinear(_ENERGY, _spec)
+    return _spec_spline
+
+def build_src_template(cat_file, psf_file, emin, emax, b_cut=20, nside=512):
     """Returns a map of sources inside the catalog smoothed by the PSF 
        at the gven energy.
 
@@ -37,30 +60,76 @@ def buils_src_template(cat_file, psf_file, energy, NSIDE):
            .fits fits file with the sorce catalog
        psf_file: str
            ascii file with 2 columns (Energy - PSF)
-       energy: float
-           center of the energy (micro) bin [MeV]
-       NSIDE: int
+       emin: float
+           minimum energy of the bin [MeV]
+       emax: float
+           maximum energy of the bin [MeV]
+       b_cut: float
+           cut on abs(b) --> only sources with abs(b)>b_cut will be
+           taken
+       nside: int
            healpix nside parameter
     """
-    psf = psf_en(psf_file, energy)
-    logger.info('ENERGY = %.2f MeV -> PSF = %.4f deg (%.f rad)'
-                %(energy, psf, np.radians(psf)))
+    psf_th_e = get_psf(psf_file)
+    emean = np.sqrt(emin*emax)
+    psf_th = psf_th_e.vslice(emean)
+    ##########################################################
     src_cat = pf.open(cat_file)
-    npix = hp.pixelfunc.nside2npix(NSIDE)
-    src_map = np.full(npix, 0)
     CAT = src_cat['LAT_Point_Source_Catalog']
     SOURCES = CAT.data
-    FLUX = SOURCES.field('Flux1000')
-    GLON = SOURCES.field('GLON')
     GLAT = SOURCES.field('GLAT')
+    logger.info('Considering |b| > %.1f'%b_cut)
+    lat_index = np.where(abs(GLAT)>b_cut)[0]
+    logger.info('Number of 3FGL sources: %i'%len(lat_index))
+    GLAT =  GLAT[lat_index]
+    GLON = SOURCES.field('GLON')[lat_index]
+    FLUX1000 = SOURCES.field('Flux1000')[lat_index]
+    FLUX30_100 = SOURCES.field('Flux30_100')[lat_index]
+    FLUX100_300 = SOURCES.field('Flux100_300')[lat_index]
+    FLUX300_1000 = SOURCES.field('Flux300_1000')[lat_index]
+    E0 = SOURCES.field('Pivot_Energy')[lat_index]
+    K = SOURCES.field('Flux_Density')[lat_index]
+    alpha = GAMMA = SOURCES.field('Spectral_Index')[lat_index]
+    spec_type = SOURCES.field('SpectrumType')[lat_index]
+    beta = SOURCES.field('beta')[lat_index]
+    b = SOURCES.field('Exp_Index')[lat_index]
+    Ecut = SOURCES.field('Cutoff')[lat_index]
+    src_name = SOURCES.field('Source_Name')[lat_index]
     src_cat.close()
-    x, y, z = hp.rotator.dir2vec(GLON,GLAT,lonlat=True)
-    src_pix= hp.pixelfunc.vec2pix(NSIDE, x, y, z)
-    for i, bn in enumerate(src_pix):
-        src_map[bn] = FLUX[i]
-    src_map = hp.sphtfunc.smoothing(src_map, fwhm=np.radians(psf/1.18))
+    ##########################################################
+    INT_FLUXES = []
+    for i, s in enumerate(spec_type):
+        spec = 0
+        if s == 'PowerLaw':
+            spec = PowerLaw(K[i], alpha[i], E0[i])
+            integral_flux_bin = spec.integral(emin, emax)
+            INT_FLUXES.append(integral_flux_bin)
+        elif s == 'LogParabola':
+            spec = LogParabola(K[i], alpha[i], beta[i], E0[i])
+            integral_flux_bin = spec.integral(emin, emax)
+            INT_FLUXES.append(integral_flux_bin)
+        else:
+            spec = PLSuperExpCutoff(K[i], alpha[i], E0[i], b[i], Ecut[i])
+            integral_flux_bin = spec.integral(emin, emax)
+            INT_FLUXES.append(integral_flux_bin)
+    logger.info('Building...')
+    src_map = src_builder(psf_th, nside, GLON, GLAT, INT_FLUXES)
+    logger.info('Done!...')
     return src_map
 
+@jit
+def src_builder(psf_spline, nside, glon_, glat_, intflux_): 
+    x, y, z = hp.rotator.dir2vec(glon_, glat_, lonlat=True)
+    src_pix = hp.pixelfunc.vec2pix(nside, x, y, z)
+    isomap = np.zeros(hp.nside2npix(nside))
+    for i, bn in enumerate(src_pix):
+        pixdir1 = hp.pixelfunc.pix2ang(nside, bn)
+        radintpix = hp.query_disc(nside, (x[i],y[i],z[i]), np.radians(10))
+        pixdir2 = hp.pixelfunc.pix2ang(nside, radintpix)
+        dist = hp.rotator.angdist(pixdir1, pixdir2)
+        psf = psf_spline(dist)
+        isomap[radintpix]= isomap[radintpix] + intflux_[i]*psf
+    return isomap
 
 def main():
     """Simple test unit
@@ -68,20 +137,28 @@ def main():
     sourcetemp = True
     
     if sourcetemp == True:
+        abslatitude = 20
         cat_file = os.path.join(GRATOOLS_CONFIG,'catalogs/gll_psc_v16.fit')
-        psf_file = os.path.join(GRATOOLS_CONFIG, 
-                                'ascii/PSF_UCV_PSF1.txt')
+        psf_file = os.path.join(GRATOOLS_OUT, 
+                                'psf_P8R2_ULTRACLEANVETO_V6_32.fits')
+        isofile = os.path.join(GRATOOLS_CONFIG, 'models', 
+                           'iso_P8R2_ULTRACLEANVETO_V6_v06.txt')
+        e_min, e_max= 1000, 1737
+        src_templ_map = build_src_template(cat_file, psf_file, emin=e_min, 
+                                           emax=e_max, b_cut=abslatitude)
+        from GRATools.utils.gForeground import get_iso_integral_flux_map
+        iso_map = get_iso_integral_flux_map(isofile, e_min, e_max)
+        iso_src_map = iso_map + src_templ_map
+        pc.dump(iso_src_map,
+                 open('SourceTempl_prova.pkl', 'wb'))     
         mask_f =  os.path.join(GRATOOLS_CONFIG, 
                               'fits/Mask_gp30.fits')
         mask = hp.read_map(mask_f)
-        energy = 1340.69
-        NSIDE = 512
-        src_templ_map = buils_src_template(cat_file, psf_file, energy, NSIDE)
-        src_templ_map_masked = hp.ma(src_templ_map)
-        src_templ_map_masked.mask = np.logical_not(mask)
-        titolo = 'Energy: %f MeV'%energy
-        hp.mollview(src_templ_map_masked.filled(), title=titolo, 
-                    coord='G', min=1e-15, max=6e-10, norm='log')
+        iso_src_map_masked = hp.ma(iso_src_map)
+        iso_src_map_masked.mask = np.logical_not(mask)
+        titolo = 'Energy: %f - %f MeV'%(e_min, e_max)
+        hp.mollview(iso_src_map_masked.filled(), title=titolo, 
+                    coord='G',norm='log')#, min=1.5e-7, max=4e-7)
         hp.graticule()
         plt.show()
         
