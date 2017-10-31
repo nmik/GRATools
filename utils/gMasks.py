26c915ef225643e0699633b529cc133f8e3c7f4b
Michela Negro <michela.negro@to.infn.it>
2017-10-31 17:01:50 +0100

    batman gp mask

--- utils/gMasks.py
+++ utils/gMasks.py
@@ -4,7 +4,7 @@
 # On behalf of the Fermi-LAT Collaboration.                                    #
 #                                                                              #
 # This program is free software; you can redistribute it and/or modify         #
-# it under the terms of the GNU General Public License as published by       #
+# it under the terms of the GNU General Public License as published by         #
 # the Free Software Foundation; either version 3 of the License, or            #
 # (at your option) any later version.                                          #
 #                                                                              #
@@ -109,6 +109,31 @@ def mask_gp(MASK_GP_LAT, NSIDE):
     lon,lat = hp.rotator.vec2dir(x,y,z,lonlat=True)
     for i,b in enumerate(lat):
         if abs(b) <= MASK_GP_LAT:
+            BAD_PIX_GP.append(iii[i])
+    return BAD_PIX_GP
+
+def mask_bat_gp(FLUX_CUT, NSIDE):
+    """Returns the 'bad pixels' around the galactic plain. 
+                      
+       FLUX_CUT: float              
+           absolute value of galactic latitude definig bad pixels to mask     
+       NSIDE: int                                   
+           healpix nside parameter                                
+    """
+    logger.info('Batman mask for the galactic plane activated')
+    flux_map = hp.read_map(os.path.join(GRATOOLS_CONFIG,
+                                 'fits/gll_iem_v06_hp512_1000-1096.fits'))
+    NPIX = hp.pixelfunc.nside2npix(NSIDE)
+    if len(flux_map) != NPIX:
+        flux_map = hp.ud_grade(flux_map, nsideout=NSIDE)
+    else:
+        pass
+    BAD_PIX_GP = []
+    iii = range(NPIX)
+    x,y,z = hp.pix2vec(NSIDE,iii)
+    lon,lat = hp.rotator.vec2dir(x,y,z,lonlat=True)
+    for i,b in enumerate(flux_map):
+        if b >= FLUX_CUT:
             BAD_PIX_GP.append(iii[i])
     return BAD_PIX_GP
 
@@ -263,52 +288,47 @@ def mask_src_weighted(cat_src_file, cat_extsrc_file, ENERGY, NSIDE):
        NSIDE: int
           healpix nside parameter
     """
-    psf_ref_file = os.path.join(GRATOOLS_CONFIG, 'ascii/PSF_UCV_PSF1.txt')
+    psf_ref_file = os.path.join(GRATOOLS_CONFIG, 'ascii/PSF_UCV_custom.txt')
     src_cat = pf.open(cat_src_file)
     extsrc_cat = pf.open(cat_extsrc_file)
     NPIX = hp.pixelfunc.nside2npix(NSIDE)
     CAT = src_cat['LAT_Point_Source_Catalog']
-    #CAT = src_cat['LAT_POINT_SOURCE_CATALOG'] 
     CAT_EXTENDED = extsrc_cat['ExtendedSources']
     BAD_PIX_SRC = []
     SOURCES = CAT.data
     EXT_SOURCES = CAT_EXTENDED.data
     src_cat.close()
     psf_ref = get_psf_ref(psf_ref_file)
-    psf_en = psf_ref(ENERGY)
-    psf_min, psf_max =  psf_ref.y[5], psf_ref.y[-1] 
-    norm_min, norm_max = 1, 0.3
-    norm = norm_min + psf_en*((norm_max - norm_min)/(psf_max - psf_min)) -\
-        psf_min*((norm_max - norm_min)/(psf_max - psf_min))
-    logger.info('Normalization of radii due to energy: %.3f'%norm)
-    logger.info('Psf(%.2f)= %.2f'%(ENERGY, psf_en))
-    #FLUX = SOURCES.field('Flux1000')
-    #FLUX = np.log10(np.sort(SOURCES.field('Flux1000')))
-    FLUX = np.log10(SOURCES.field('Flux1000'))
-    #FLUX = np.log10(SOURCES.field('Flux_Density'))
+    if '4FGL' in cat_src_file:
+        FLUX = np.log10(SOURCES.field('Flux_Density'))
+    else:
+        FLUX = np.log10(SOURCES.field('Flux1000'))
     flux_min, flux_max = min(FLUX), max(FLUX)
-    rad_min, rad_max = 1, 5.
+    rad_min = 2*psf_ref(ENERGY)
+    rad_max = 5*psf_ref(ENERGY)
     RADdeg = rad_min + FLUX*((rad_max - rad_min)/(flux_max - flux_min)) -\
         flux_min*((rad_max - rad_min)/(flux_max - flux_min))
     RADrad = np.radians(RADdeg)
     logger.info('Masking the extended Sources')
-    logger.info('-> 10deg around CenA and LMC')
-    logger.info('-> 5deg around the remaining')
+    logger.info('-> 10xPSF(%.2f GeV) around CenA and LMC'%(ENERGY/1000))
+    logger.info('-> 5xPSF(%.2f GeV) around the remaining'%(ENERGY/1000))
     for i, src in enumerate(EXT_SOURCES):
         NAME = EXT_SOURCES[i][0]
         GLON = EXT_SOURCES.field('GLON')[i]
         GLAT = EXT_SOURCES.field('GLAT')[i]
         if 'LMC' in NAME or 'CenA Lobes' in NAME:
+            rad = 10*psf_ref(ENERGY)
             x, y, z = hp.rotator.dir2vec(GLON,GLAT,lonlat=True)
             b_pix= hp.pixelfunc.vec2pix(NSIDE, x, y, z)
             BAD_PIX_SRC.append(b_pix) 
-            radintpix = hp.query_disc(NSIDE, (x, y, z), np.radians(10)*norm)
+            radintpix = hp.query_disc(NSIDE, (x, y, z), np.radians(10))
             BAD_PIX_SRC.extend(radintpix)
         else:
+            rad = 5*psf_ref(ENERGY)
             x, y, z = hp.rotator.dir2vec(GLON,GLAT,lonlat=True)
             b_pix = hp.pixelfunc.vec2pix(NSIDE, x, y, z)
             BAD_PIX_SRC.append(b_pix) 
-            radintpix = hp.query_disc(NSIDE, (x, y, z), np.radians(5)*norm)
+            radintpix = hp.query_disc(NSIDE, (x, y, z), np.radians(5))
             BAD_PIX_SRC.extend(radintpix)
     logger.info('Flux-weighted mask for sources activated')
     for i, src in enumerate(SOURCES):
@@ -317,7 +337,7 @@ def mask_src_weighted(cat_src_file, cat_extsrc_file, ENERGY, NSIDE):
         x, y, z = hp.rotator.dir2vec(GLON,GLAT,lonlat=True)
         b_pix= hp.pixelfunc.vec2pix(NSIDE, x, y, z)
         BAD_PIX_SRC.append(b_pix) 
-        radintpix = hp.query_disc(NSIDE, (x, y, z), RADrad[i]*norm)
+        radintpix = hp.query_disc(NSIDE, (x, y, z), RADrad[i])
         BAD_PIX_SRC.extend(radintpix)
     return BAD_PIX_SRC
 
@@ -331,14 +351,15 @@ def main():
     CAT = src_cat['LAT_Point_Source_Catalog']
     SOURCES = CAT.data
     src_cat.close()
-    flux = np.log10(np.sort(SOURCES.field('Flux1000')))
+    flux = np.sort(SOURCES.field('Flux1000'))
     flux_min, flux_max = min(flux), max(flux)
+    """
     rad_min, rad_max = 0.5, 5.
     RADdeg = rad_min + flux*((rad_max - rad_min)/(flux_max - flux_min)) -\
         flux_min*((rad_max - rad_min)/(flux_max - flux_min))
     plt.figure(facecolor='white')
     plt.title('Disk Radius as a function of $\Phi_{src}$')
-    plt.plot(10**flux, RADdeg, '*-', color='cornflowerblue', linewidth=1, 
+    plt.plot(flux, RADdeg, '*-', color='cornflowerblue', linewidth=1, 
              ms=5, alpha=0.2)
     plt.xlabel('Flux [photon/cm$^{2}$/s]')
     plt.ylabel('mask disk Radius [$^{\circ}$]')
@@ -346,39 +367,46 @@ def main():
     plt.xscale('log')
     plt.ylim(1.5, 6)
     plt.grid(which='both', linestyle='--', linewidth=1)
-
+    """
+    """
     from GRATools.utils.gWindowFunc import get_psf_ref
-    psf_ref_file = os.path.join(GRATOOLS_CONFIG, 'ascii/PSF_UCV_PSF1.txt')
+    psf_ref_file = os.path.join(GRATOOLS_CONFIG, 'ascii/PSF_UCV_custom.txt')
     psf_ref = get_psf_ref(psf_ref_file)
     energy = np.logspace(1, 6, 10)
-    psf_en = psf_ref(energy)
-    psf_min, psf_max =  psf_ref.y[5], psf_ref.y[-1] 
-    norm_min, norm_max = 1, 0.3
-    norm = norm_min + psf_en*((norm_max - norm_min)/(psf_max - psf_min)) -\
-        psf_min*((norm_max - norm_min)/(psf_max - psf_min))
-    norm_interp = interp1d(energy, norm, kind='cubic')
-    plt.figure(facecolor='white')
-    plt.title('Normalization Factor')
-    plt.plot(energy, norm, '-', color='cornflowerblue',  ms=4, alpha=0.75)
-    plt.xlabel('Energy [MeV]')
-    plt.ylabel('PSF normalization factor')
-    plt.yscale('log')
-    plt.xscale('log')
-    plt.ylim(0.09, 5)
-    plt.xlim(100, 1000000)
-    plt.grid(which='both', linestyle='--', linewidth=1)
-    #plt.show()
-    
+    energy = [301.99,524.81,1000.00,2754.23,
+              8317.64,22908.68,331131.12]
+    lab = ['PSF3','PSF3','PSF1+2+3', 'PSF1+2+3', 'PSF1+2+3','PSF1+2+3', 
+           'PSF1+2+3']
+    plt.figure(facecolor='white')  
+    for i, en in enumerate(energy):
+        r_min = 2*psf_ref(en)
+        r_max = 5*psf_ref(en)
+        print 'R min', r_min, 'R max', r_max
+        print 'F min', flux_min, 'F max', flux_max
+        RADdeg = r_min + np.log10(flux)*((r_max - r_min)/(np.log10(flux_max) - np.log10(flux_min))) -\
+        np.log10(flux_min)*((r_max - r_min)/(np.log10(flux_max) - np.log10(flux_min)))
+        #plt.figure(facecolor='white')
+        plt.title('Disk Radius as a function of $\Phi_{src}$')
+        plt.plot(np.log10(flux), RADdeg, 'o-', linewidth=1,
+                 ms=3, alpha=0.4, label="%.1f GeV (%s)"%(en/1000., lab[i]))
+        plt.xlabel('Flux [photon/cm$^{2}$/s]')
+        plt.ylabel('Mask disk Radius [$^{\circ}$]')
+        #plt.yscale('log')
+        #plt.xscale('log')
+        #plt.ylim(1.5, 6)
+        plt.grid(which='both', linestyle='--', linewidth=1)
+    plt.legend(fontsize=11, loc=2)
+    """
     nside = 512
     SRC_CATALOG_FILE = os.path.join(GRATOOLS_CONFIG,'catalogs/gll_psc_v16.fit')
-    bad_pix = mask_src_weighted(SRC_CATALOG_FILE, 10000, nside)
-    bad_pix += mask_gp(30, nside)
+    bad_pix = mask_src_weighted(SRC_CATALOG_FILE, SRC_CATALOG_FILE, 1000, nside)
+    bad_pix += mask_bat_gp(2e-7, nside)
     npix = hp.nside2npix(nside)
     mask = np.ones(npix)
     for bpix in np.unique(bad_pix):
         mask[bpix] = 0
     fsky = 1-(len(np.unique(bad_pix))/float(npix))
-    title = 'Energy = 10 GeV, F$_{sky}$ = %.3f'%(fsky)
+    title = 'Energy = 100 GeV, F$_{sky}$ = %.3f'%(fsky)
     hp.mollview(mask, title=title, coord='G', cmap='bone')
     hp.graticule(color='silver')
     plt.show()
